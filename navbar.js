47a0cfb2b1d1af49080bafb0074a6560656d1c19
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-10-02 15:51:46 +0800

    Update navbar.js
    updated nav bar

--- navbar.js
+++ navbar.js
@@ -27,20 +27,49 @@ export default /*html*/`
             </form>
           </li>
         </ul>
-        <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-            <!-- ternary-conditional-operator, checking if not null, then welcome, otherwise a fallback -->
-            ${user_category ? 'Welcome, ' + user_category : 'Profile'}
-          </button>
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#">My Learning Journey</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
-          </ul>
-        </div>
+        ${user_category
+          // if user category not null, dropdown with welcome/category
+          ? /*html*/`
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Welcome, ${user_category}
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">My Learning Journey</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Logout</a></li>
+            </ul>
+          </div>
+          `
+          // if user category is null
+          : /*html*/`
+          <div>
+            <button class="btn btn-secondary" type="button">
+              <!-- currently nothing added yet -->
+              Login
+            </button>
+          </div>
+          `
+        }
       </div>
       <div class="col-2"></div>
     </div>
   </nav>
 `;
+
+
+
+/*
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    <!-- ternary-conditional-operator, checking if not null, then welcome, otherwise a fallback -->
+    ${user_category ? 'Welcome, ' + user_category : 'Profile'}
+  </button>
+  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">My Learning Journey</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Logout</a></li>
+  </ul>
+</div>
+*/
 
